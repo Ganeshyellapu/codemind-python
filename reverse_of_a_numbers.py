2bd11200909dce1ed6f0140689cfa2f76f3115e1
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-11 10:53:46 +0530

    n=int(input()) q=n s=0 if q<0:     q=q*-1     while q:         r=q%10         s=s*10+r         q=q//10     print(s*-1) else:     while(q):         r=q%10         s=s*10+r         q=q//10     print(s)

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,3 +1,16 @@
-n=str(input())
-r=n[::-1]
-print(r)
+n=int(input())
+q=n
+s=0
+if q<0:
+    q=q*-1
+    while q:
+        r=q%10
+        s=s*10+r
+        q=q//10
+    print(s*-1)
+else:
+    while(q):
+        r=q%10
+        s=s*10+r
+        q=q//10
+    print(s)
