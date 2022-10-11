2f9398ee71ca62d53e5a1d2161a6fa7f6db168e5
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-11 10:30:18 +0530

    def pretty(x,y):     if x==0:         return False     elif y%x==0:         return True     else:         return False a=int(input()) b=int(input()) for i in range(a,b+1):     c=0     k=0     q=i     while(q):         r=q%10         c=c+1         if pretty(r,i):             k=k+1         q=q//10     if c==k:         print(i,end=" ")     c=0     k=0

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,14 +1,23 @@
+def pretty(x,y):
+    if x==0:
+        return False
+    elif y%x==0:
+        return True
+    else:
+        return False
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    s=0
-    c=len(str(i))
+    c=0
+    k=0
     q=i
     while(q):
         r=q%10
-        if r!=0:
-            if i%r==0:
-                s=s+1
+        c=c+1
+        if pretty(r,i):
+            k=k+1
         q=q//10
-    if(s==c):
-        print(i,end=' ')
+    if c==k:
+        print(i,end=" ")
+    c=0
+    k=0
